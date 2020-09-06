08d726afc73dfa0717d8fbffa277446ce04e269d
huemic73 <68350531+huemic73@users.noreply.github.com>
2020-09-06 16:18:27 +0200

    changed port

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -16,7 +16,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {    
-    fetch('http://localhost:3000/signin', {
+    fetch('https://secret-stream-01698.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
